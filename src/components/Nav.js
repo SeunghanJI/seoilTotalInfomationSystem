c9f6eecbdc3341a1c24e9d784f25c676e03619b6
yuntaengtaeng <k76253218@gmail.com>
2021-11-25 22:54:41 +0900

    feat : Nav 컴포넌트 아이템 클릭 시 URL 이동 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 
+import { useNavigate } from 'react-router-dom';
+
 const { Header } = Layout;
 const { SubMenu } = Menu;
 
-const nav = () => {
+const Nav = () => {
+  const navigate = useNavigate();
+
+  const openNewWindow = (type) => {
+    const URL = (() => {
+      switch (type) {
+        case 'E-Campus': {
+          return 'https://ecampus.seoil.ac.kr/local/intro/main.php';
+        }
+        case 'NCS': {
+          return 'https://ncs.seoil.ac.kr/';
+        }
+        case '전자출결시스템': {
+          return 'https://attend.seoil.ac.kr/';
+        }
+        case '수강신청': {
+          return 'https://sugang.seoil.ac.kr/';
+        }
+
+        default:
+          return null;
+      }
+    })();
+
+    if (URL) {
+      window.open(URL, '_blank');
+    }
+  };
+
   return (
     <Header
       theme="light"
@@ -16,7 +46,16 @@ const nav = () => {
       }}
     >
       <div
-        style={{ display: 'flex', alignItems: 'center', whiteSpace: 'nowrap' }}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          whiteSpace: 'nowrap',
+          fontSize: '14px',
+          fontWeight: 'bold',
+        }}
+        onClick={() => {
+          navigate('/');
+        }}
       >
         <img
           src={`${process.env.PUBLIC_URL}/logo.png`}
@@ -44,14 +83,44 @@ const nav = () => {
             <Menu.Item key="setting2:4">취업이력사항</Menu.Item>
           </Menu.ItemGroup>
         </SubMenu>
-        <SubMenu key="SubMenu2" title="E-Campus"></SubMenu>
-        <SubMenu key="SubMenu3" title="NCS"></SubMenu>
-        <SubMenu key="SubMenu4" title="전자출결시스템"></SubMenu>
-        <SubMenu key="SubMenu5" title="수강신철"></SubMenu>
-        <SubMenu key="SubMenu6" title="내 정보 관리"></SubMenu>
+        <SubMenu
+          key="SubMenu2"
+          title="E-Campus"
+          onTitleClick={() => {
+            openNewWindow('E-Campus');
+          }}
+        ></SubMenu>
+        <SubMenu
+          key="SubMenu3"
+          title="NCS"
+          onTitleClick={() => {
+            openNewWindow('NCS');
+          }}
+        ></SubMenu>
+        <SubMenu
+          key="SubMenu4"
+          title="전자출결시스템"
+          onTitleClick={() => {
+            openNewWindow('전자출결시스템');
+          }}
+        ></SubMenu>
+        <SubMenu
+          key="SubMenu5"
+          title="수강신청"
+          onTitleClick={() => {
+            openNewWindow('수강신청');
+          }}
+        ></SubMenu>
+        <SubMenu
+          key="SubMenu6"
+          title="내 정보 관리"
+          onTitleClick={() => {
+            navigate('/profile');
+          }}
+        ></SubMenu>
       </Menu>
     </Header>
   );
 };
 
-export default nav;
+export default Nav;
