268a7c1615bf2e1544c18ae4fdb0834af67e81c9
yuntae-go <dbsxo7625@naver.com>
2021-11-26 14:36:53 +0900

    feat : App.js 레이아웃 구조 변경, footer 추기
    - Nav 컴포넌트 구조, 구성 컴포넌트 요소 변경, 메뉴 항목 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import './App.css';
+
 import Nav from './components/Nav';
+import NotFound from './pages/NotFound';
 
 import { ROUTES } from './routes';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+import { Layout } from 'antd';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const loginCallBack = (isLoggedStatus) => {
+    setIsLogin(isLoggedStatus);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav></Nav>
-        <section>
-          <Routes>
-            {ROUTES.map((route) => {
-              return (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={<route.component />}
-                ></Route>
-              );
-            })}
-            {/* <Route component={NotFound} /> */}
-          </Routes>
-        </section>
+        <Layout style={{ minHeight: '100vh', backgroundColor: '#fff' }}>
+          <Header
+            theme="light"
+            style={{
+              padding: '0px 20px',
+              width: '100%',
+              background: '#fff',
+            }}
+          >
+            <Nav></Nav>
+          </Header>
+          <Content style={{ width: '80%', margin: '0 auto' }}>
+            <Routes>
+              {ROUTES.map((route) => {
+                return (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={
+                      <route.component
+                        loginCallBack={loginCallBack}
+                        isLogin={isLogin}
+                      />
+                    }
+                  ></Route>
+                );
+              })}
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </Content>
+          <Footer style={{ marginTop: '48px' }}></Footer>
+        </Layout>
       </div>
     </BrowserRouter>
   );
