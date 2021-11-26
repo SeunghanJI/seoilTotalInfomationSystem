1f9f683b05ff8b3496e19583f7174f54e2e08837
yuntaengtaeng <k76253218@gmail.com>
2021-11-26 14:36:53 +0900

    feat : App.js 레이아웃 구조 변경, footer 추기
    - Nav 컴포넌트 구조, 구성 컴포넌트 요소 변경, 메뉴 항목 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Menu } from 'antd';
 
 import { useNavigate } from 'react-router-dom';
-
-const { Header } = Layout;
-const { SubMenu } = Menu;
 
 const Nav = () => {
   const navigate = useNavigate();
@@ -36,17 +33,10 @@ const Nav = () => {
   };
 
   return (
-    <Header
-      theme="light"
-      style={{
-        padding: '0px 20px',
-        width: '100%',
-        display: 'flex',
-        background: '#fff',
-      }}
-    >
+    <>
       <div
         style={{
+          float: 'left',
           display: 'flex',
           alignItems: 'center',
           whiteSpace: 'nowrap',
@@ -64,62 +54,65 @@ const Nav = () => {
         />
         서일대학교 종합정보 시스템
       </div>
-      <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
-        <SubMenu key="SubMenu1" title="학사정보">
+      <Menu mode="horizontal">
+        <Menu.SubMenu key="학사정보" title="학사정보">
           <Menu.ItemGroup title="학사 서비스">
-            <Menu.Item key="setting1:1">입대휴학신청</Menu.Item>
-            <Menu.Item key="setting1:2">복학신청</Menu.Item>
-            <Menu.Item key="setting1:3">
-              계절학기(환불)신청/고지서 출력
-            </Menu.Item>
-            <Menu.Item key="setting1:4">교차수강신청</Menu.Item>
-            <Menu.Item key="setting1:5">수강신청서출력</Menu.Item>
-            <Menu.Item key="setting1:6">강의평가</Menu.Item>
+            <Menu.Item key="입대휴학신청">입대휴학신청</Menu.Item>
+            <Menu.Item key="복학신청">복학신청</Menu.Item>
+            <Menu.Item key="수강신청서출력">수강신청서출력</Menu.Item>
+            <Menu.Item key="강의평가">강의평가</Menu.Item>
+            <Menu.Item key="개설강좌 조회">개설강좌 조회</Menu.Item>
+            <Menu.Item key="성적조회">성적조회</Menu.Item>
           </Menu.ItemGroup>
           <Menu.ItemGroup title="학생 서비스">
-            <Menu.Item key="setting2:1">장학증서 출력</Menu.Item>
-            <Menu.Item key="setting2:2">도우미 활동</Menu.Item>
-            <Menu.Item key="setting2:3">해외봉사 활동</Menu.Item>
-            <Menu.Item key="setting2:4">취업이력사항</Menu.Item>
+            <Menu.Item key="장학증서 출력">장학증서 출력</Menu.Item>
+            <Menu.Item key="도우미 활동">도우미 활동</Menu.Item>
+            <Menu.Item key="해외봉사 활동">해외봉사 활동</Menu.Item>
+            <Menu.Item key="취업이력사항">취업이력사항</Menu.Item>
           </Menu.ItemGroup>
-        </SubMenu>
-        <SubMenu
-          key="SubMenu2"
-          title="E-Campus"
-          onTitleClick={() => {
+        </Menu.SubMenu>
+        <Menu.Item
+          key="E-Campus"
+          onClick={() => {
             openNewWindow('E-Campus');
           }}
-        ></SubMenu>
-        <SubMenu
-          key="SubMenu3"
-          title="NCS"
-          onTitleClick={() => {
+        >
+          E-Campus
+        </Menu.Item>
+        <Menu.Item
+          key="NCS"
+          onClick={() => {
             openNewWindow('NCS');
           }}
-        ></SubMenu>
-        <SubMenu
-          key="SubMenu4"
-          title="전자출결시스템"
-          onTitleClick={() => {
+        >
+          NCS
+        </Menu.Item>
+        <Menu.Item
+          key="전자출결시스템"
+          onClick={() => {
             openNewWindow('전자출결시스템');
           }}
-        ></SubMenu>
-        <SubMenu
-          key="SubMenu5"
-          title="수강신청"
-          onTitleClick={() => {
+        >
+          전자출결시스템
+        </Menu.Item>
+        <Menu.Item
+          key="수강신청"
+          onClick={() => {
             openNewWindow('수강신청');
           }}
-        ></SubMenu>
-        <SubMenu
-          key="SubMenu6"
-          title="내 정보 관리"
-          onTitleClick={() => {
+        >
+          수강신청
+        </Menu.Item>
+        <Menu.Item
+          key="내 정보 관리"
+          onClick={() => {
             navigate('/profile');
           }}
-        ></SubMenu>
+        >
+          내 정보 관리
+        </Menu.Item>
       </Menu>
-    </Header>
+    </>
   );
 };
 
