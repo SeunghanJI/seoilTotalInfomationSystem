1d8830eba3e8bba89e74e6487d7a0bf851e7b37d
yuntae-go <dbsxo7625@naver.com>
2021-11-28 21:36:11 +0900

    feat : 내 정보 관리 프로필 페이지 컴포넌트 추가
    - 로그인이 되어 있지 않다면 로그인 폼 컴포넌트 되어있다면 프로필 정보 렌더링

---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import LoginForm from '../../components/LoginForm';
+
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
+
+const Profile = ({ isLogin, loginCallBack }) => {
+  const drawProfile = () => {
+    return (
+      <Tabs defaultActiveKey="1" tabPosition="left">
+        <TabPane tab="개인정보 관리 (현)개인신상카드" key="1"></TabPane>
+        <TabPane tab="장학금 계좌 관리" key="2"></TabPane>
+        <TabPane tab="계정 정보관리" key="3"></TabPane>
+      </Tabs>
+    );
+  };
+
+  const drawLoginForm = () => {
+    return (
+      <div style={{ width: '60%', margin: 'auto' }}>
+        <h2 style={{ textAlign: 'center' }}>로그인을 해주세요</h2>
+        <LoginForm loginCallBack={loginCallBack}></LoginForm>
+      </div>
+    );
+  };
+
+  return <>{isLogin ? drawProfile() : drawLoginForm()}</>;
+};
+
+export default Profile;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import Main from './pages/Main';
+import Profile from './pages/profile/Profile';
 
 export const ROUTES = [
   {
     path: '/',
     component: Main,
+  },
+  {
+    path: '/profile',
+    component: Profile,
   },
 ];
