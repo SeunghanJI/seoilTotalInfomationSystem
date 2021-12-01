7bbd14de8926289a5f85854a0143f8c5d70466d9
yuntae-go <dbsxo7625@naver.com>
2021-12-01 18:11:10 +0900

    feat : Nav 컴포넌트 학사정보 항목 information 페이지 컴포넌트 연결
    - antd Menu selectedKeys를 사용하여 현재 URL을 이용하여 active 되도록 변경
    - information page 컴포넌트 추가

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Main from './pages/Main';
 import Profile from './pages/profile/Profile';
+import Information from './pages/information/Information';
 
 export const ROUTES = [
   {
@@ -9,5 +10,9 @@ export const ROUTES = [
   {
     path: '/profile',
     component: Profile,
+  },
+  {
+    path: '/Information/:target',
+    component: Information,
   },
 ];
