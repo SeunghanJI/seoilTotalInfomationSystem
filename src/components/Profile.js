ab2254fe17f9ad0b02480bebfa56c41a07f251d0
yuntaengtaeng <k76253218@gmail.com>
2021-12-01 18:05:39 +0900

    feat : 프로필 컴포넌트 API 연결

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/';
+
 const Profile = () => {
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('api/user/simple')
+      .then(({ data }) => {
+        setProfileData(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
   return (
-    <div>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
       <Avatar size={48} icon={<UserOutlined />} />
-      <div>
-        <p>
-          이름 : <span>지승한</span>
-        </p>
-        <p>
-          학번 : <span>201800001</span>
-        </p>
-      </div>
+      {profileData && (
+        <div style={{ marginTop: '16px' }}>
+          <p>
+            이름 : <span>{profileData.name}</span>
+          </p>
+          <p>
+            학번 : <span>{profileData.id}</span>
+          </p>
+          <p>
+            학과 : <span>{profileData.deptName}</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
