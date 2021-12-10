8cf3818ee59316aa191cd276153bbd8347d61e24
yuntaengtaeng <k76253218@gmail.com>
2021-12-11 03:44:46 +0900

    feat : 내 정보 관리 개인정보 관리 신상카드 탭 컴포넌트 추가
    - 개인 정보 get, 수정 fetch api 연결

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LoginForm from '../../components/LoginForm';
+import Privacy from './Privacy';
 
 import { Tabs } from 'antd';
 const { TabPane } = Tabs;
@@ -9,7 +10,9 @@ const Profile = ({ isLogin, loginCallBack }) => {
   const drawProfile = () => {
     return (
       <Tabs defaultActiveKey="1" tabPosition="left">
-        <TabPane tab="개인정보 관리 (현)개인신상카드" key="1"></TabPane>
+        <TabPane tab="개인정보 관리 (현)개인신상카드" key="1">
+          <Privacy></Privacy>
+        </TabPane>
         <TabPane tab="장학금 계좌 관리" key="2"></TabPane>
         <TabPane tab="계정 정보관리" key="3"></TabPane>
       </Tabs>
