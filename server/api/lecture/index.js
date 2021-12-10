6be534162e74bdea69780e15a856d23278fcd2f4
SeunghanJI <jsh3581482@naver.com>
2021-12-10 19:41:52 +0900

    fix : 테이블구조변경에 의한 강의목록api로직변경

--- server/api/lecture/index.js
+++ server/api/lecture/index.js
@@ -17,13 +17,6 @@ const thisTerm = () => {
     return { year, term };
 };
 
-const checkId = id => {
-    if (100000000 < id && id < 999999999)
-        return 'student';
-    else
-        return 'professor';
-};
-
 const getId = session => {
     return new Promise((resolve, reject) => {
         db.get(`select id from account
@@ -40,23 +33,7 @@ const getId = session => {
     });
 };
 
-const getInfoWithId = (tableName, id) => {
-    return new Promise((resolve, reject) => {
-        db.get(`select * from ${tableName}
-                where id = ${id}`,
-            [], (err, row) => {
-                if (err) {
-                    reject(ERROR_CODE[500]);
-                }
-                if (!row) {
-                    reject(ERROR_CODE[400])
-                }
-                resolve(row);
-            });
-    });
-};
-
-const getLecturesWithTerm = (term) => {
+const getLecturesWithTerm = (year, term) => {
     return new Promise((resolve, reject) => {
         db.all(`select lecture.id as lectureId,lecture.name as lectureName ,professor.name as professorName, term, dept.name as deptName , max_personnel as maxPersonnel
                 from lecture
@@ -64,7 +41,8 @@ const getLecturesWithTerm = (term) => {
                 on lecture.prof_id = professor.id
                 inner join dept
                 on lecture.dept_id = dept.code
-                where term = "${term}"`,
+                where year = "${year}"
+                and term = "${term}"`,
             [], (err, rows) => {
                 if (err) {
                     reject(ERROR_CODE[500]);
@@ -74,11 +52,11 @@ const getLecturesWithTerm = (term) => {
     });
 };
 
-const getStudentLectures = (userId, term) => {
+const getStudentLectures = (userId, year, term) => {
     return new Promise((resolve, reject) => {
-        db.all(`select lectureId, lectureName, professorName, deptName, term, maxPersonnel from grade
+        db.all(`select lectureId, lectureName, professorName, deptName, year, term, maxPersonnel from grade
                 inner join 
-                (select lecture.id as lectureId,lecture.name as lectureName ,professor.name as professorName, term, dept.name as deptName , max_personnel as maxPersonnel
+                (select lecture.id as lectureId,lecture.name as lectureName ,professor.name as professorName, year, term, dept.name as deptName , max_personnel as maxPersonnel
                 from lecture
                 inner join professor
                 on lecture.prof_id = professor.id
@@ -86,6 +64,7 @@ const getStudentLectures = (userId, term) => {
                 on lecture.dept_id = dept.code) as lecture
                 on grade.lecture_id = lecture.lectureId
                 where student_id = ${userId}
+                and year = "${year}"
                 and term = "${term}"`,
             [], (err, rows) => {
                 if (err) {
@@ -112,30 +91,33 @@ const insertLecture = (lectureInfo) => {
 app.get('/', (req, res) => {
     const cookie = req.headers.cookie;
     const session = utils.getSession(cookie);
-    const query = req.query;
-    const year = query.year || thisTerm().year;
-    const term = query.term || thisTerm().term;
+    const query = !!Object.keys(req.query).length ? req.query : thisTerm();
+    const year = query.year;
+    const term = query.term;
+
+    console.log(req.query);
 
     if (!cookie || !session) {
         return res.status(ERROR_CODE[401].code).json(ERROR_CODE[401].message);
     }
 
-    getLecturesWithTerm([year, term].join('-'))
+    getLecturesWithTerm(year, term)
         .then(lectures => {
             res.status(200).json(lectures);
         })
         .catch(failed => {
             res.status(failed.code).json(failed.message);
-        })
+        });
 
 });
 
 app.get('/user', (req, res) => {
     const cookie = req.headers.cookie;
     const session = utils.getSession(cookie);
-    const query = req.query || thisTerm();
+    const query = !!Object.keys(req.query).length ? req.query : thisTerm();
     const year = query.year;
     const term = query.term;
+    console.log(year, term)
 
     if (!cookie || !session) {
         return res.status(ERROR_CODE[401].code).json(ERROR_CODE[401].message);
@@ -143,17 +125,14 @@ app.get('/user', (req, res) => {
 
     getId(session)
         .then(user => {
-            return getInfoWithId(checkId(user.id), user.id);
-        })
-        .then(userInfo => {
-            return getStudentLectures(userInfo.id, [year, term].join('-'));
+            return getStudentLectures(user.id, year, term);
         })
         .then(userLectures => {
             res.status(200).json(userLectures);
         })
         .catch(failed => {
             res.status(failed.code).json(failed.message);
-        })
+        });
 });
 
 app.post('/', (req, res) => {
@@ -165,7 +144,7 @@ app.post('/', (req, res) => {
         })
         .catch(failed => {
             res.status(failed.code).json(failed.message);
-        })
+        });
 })
 
 module.exports = app;
