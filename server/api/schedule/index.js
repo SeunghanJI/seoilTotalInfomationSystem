6479f021c5b0302b9d345d96b8d7f5b9bd3a8e34
SeunghanJI <jsh3581482@naver.com>
2021-12-22 18:17:52 +0900

    feat : 학사일정캘린더 불러오기 API 구현

---
+++ server/api/schedule/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const utils = require('../../utils');
+const dayjs = require('dayjs');
+const { ERROR_CODE } = require('../../errors');
+const { Select } = require('antd');
+const res = require('express/lib/response');
+
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: 'db/main.db'
+    },
+    useNullAsDefault: true
+});
+
+app.get('/calendar', (req, res) => {
+    const yearMonth = req.query.yearMonth || dayjs().format('YYYYMM');
+
+    knex('schedule')
+        .select('contents', 'date')
+        .where('date', 'like', `${yearMonth}%`)
+        .then(scheduleArr => {
+            scheduleArr = scheduleArr.reduce((scheduleObj, schedule) => {
+                scheduleObj[schedule.date] = schedule.contents;
+                return scheduleObj;
+            }, {})
+            res.status(200).json(scheduleArr);
+        })
+        .catch(ignore => {
+            res.status(500).json(ERROR_CODE[500].message);
+        });
+});
+
+module.exports = app;
