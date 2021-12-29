eaf88cb0ced35fa93b056685bf4d522c6f509445
yuntaengtaeng <k76253218@gmail.com>
2021-12-29 23:03:19 +0900

    feat : 장학금 계좌 관리 탭 추가
    - API 연결 및 기능 구현

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LoginForm from '../../components/LoginForm';
 import Privacy from './Privacy';
 import AccountInformationManagement from './AccountInformationManagement';
+import BankAccountManagement from './BankAccountManagement';
 
 import { Tabs } from 'antd';
 const { TabPane } = Tabs;
@@ -14,7 +15,9 @@ const Profile = ({ isLogin, loginCallBack }) => {
         <TabPane tab="개인정보 관리 (현)개인신상카드" key="1">
           <Privacy></Privacy>
         </TabPane>
-        <TabPane tab="장학금 계좌 관리" key="2"></TabPane>
+        <TabPane tab="장학금 계좌 관리" key="2">
+          <BankAccountManagement></BankAccountManagement>
+        </TabPane>
         <TabPane tab="계정 정보 관리" key="3">
           <AccountInformationManagement></AccountInformationManagement>
         </TabPane>
